bc25a8e17408677b4db1a5343afeea54cd7f31b8
Laszlo Teveli <tevelee@gmail.com>
2023-04-27 15:24:11 +0200

    Demonstrating compiler error

--- Sources/VariadicGenerics/VariadicGenerics.swift
+++ Sources/VariadicGenerics/VariadicGenerics.swift
@@ -1,6 +1,30 @@
-public struct VariadicGenerics {
-    public private(set) var text = "Hello, World!"
+protocol ServiceResolver {
+    func resolve<T>() -> T
+}
 
-    public init() {
+// before variadic generics...
+extension ServiceResolver {
+    func instance<T, P0>(from initializer: (P0) -> T) -> T {
+        return initializer(resolve())
+    }
+
+    func instance<T, P0, P1>(from initializer: (P0, P1) -> T) -> T {
+        return initializer(resolve(), resolve())
+    }
+
+    func instance<T, P0, P1, P2>(from initializer: (P0, P1, P2) -> T) -> T {
+        return initializer(resolve(), resolve(), resolve())
+    }
+
+    func instance<T, P0, P1, P2, P3>(from initializer: (P0, P1, P2, P3) -> T) -> T {
+        return initializer(resolve(), resolve(), resolve(), resolve())
     }
 }
+
+// after variadic generics...
+extension ServiceResolver {
+    func instance<T, each P>(from initializer: (repeat each P) -> T) -> T {
+        initializer(repeat resolve() as each P)
+    }
+}
+
--- Tests/VariadicGenericsTests/VariadicGenericsTests.swift
+++ Tests/VariadicGenericsTests/VariadicGenericsTests.swift
@@ -2,10 +2,48 @@ import XCTest
 @testable import VariadicGenerics
 
 final class VariadicGenericsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(VariadicGenerics().text, "Hello, World!")
+    func testResolution() {
+        // Given
+        let container = DummyContainer()
+        container.register(42)
+        let resolver: ServiceResolver = container
+
+        // When
+        let resolved = resolver.resolve() as Int
+
+        // Then
+        XCTAssertEqual(resolved, 42)
+    }
+
+    func testInstance() {
+        // Given
+        struct Dummy: Equatable {
+            let value: Int
+        }
+
+        let container = DummyContainer()
+        container.register(42)
+        let resolver: ServiceResolver = container
+
+        // When
+        let resolved = resolver.instance(from: Dummy.init)
+
+        // Then
+        XCTAssertEqual(resolved, Dummy(value: 42))
+    }
+}
+
+private final class DummyContainer: ServiceResolver {
+    private var instances: [ObjectIdentifier: Any] = [:]
+
+    func register<T>(_ instance: T) {
+        instances[ObjectIdentifier(T.self)] = instance
+    }
+
+    func resolve<T>() -> T {
+        guard let instance = instances[ObjectIdentifier(T.self)] as? T else {
+            fatalError("not registered")
+        }
+        return instance
     }
 }
